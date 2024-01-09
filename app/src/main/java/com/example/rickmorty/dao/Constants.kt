35bbbbe819e6a7ffad27b91003c95567960d09dc
chungvu <chung.vu@chudu24.com>
2024-01-09 17:11:46 +0700

    code

---
+++ app/src/main/java/com/example/rickmorty/dao/Constants.kt
@@ -0,0 +1,5 @@
+package com.example.rickmorty.dao
+
+object Constants {
+    const val BASE_URL = "https://rickandmortyapi.com/api/"
+}
--- app/src/main/java/com/example/rickmorty/dao/HttpClient.kt
+++ app/src/main/java/com/example/rickmorty/dao/HttpClient.kt
@@ -4,10 +4,9 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object HttpClient {
-    private const val BASE_URL = "https://rickandmortyapi.com/api/"
 
     private val retrofit: Retrofit = Retrofit.Builder()
-        .baseUrl(BASE_URL)
+        .baseUrl(Constants.BASE_URL)
         .addConverterFactory(GsonConverterFactory.create())
         .build()
 
